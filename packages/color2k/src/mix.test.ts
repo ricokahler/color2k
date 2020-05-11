7d0a5462d2cfdbefbcf5410ec474ac13d42058fa
Rico Kahler <ricokahler@me.com>
2020-05-11 15:47:25 -0400

    swap mix args

--- packages/color2k/src/mix.test.ts
+++ packages/color2k/src/mix.test.ts
@@ -19,11 +19,11 @@ test('mix red with blue', () => {
 });
 
 test('weight 100%', () => {
-  expect(mix('red', 'blue', 1)).toMatchInlineSnapshot(`"rgba(255, 0, 0, 1)"`);
+  expect(mix('red', 'blue', 1)).toMatchInlineSnapshot(`"rgba(0, 0, 255, 1)"`);
 });
 
 test('weight 0%', () => {
-  expect(mix('red', 'blue', 0)).toMatchInlineSnapshot(`"rgba(0, 0, 255, 1)"`);
+  expect(mix('red', 'blue', 0)).toMatchInlineSnapshot(`"rgba(255, 0, 0, 1)"`);
 });
 
 test('mix with transparent', () => {
@@ -36,4 +36,18 @@ test('mix with transparent', () => {
   expect(mix('rgba(255, 0, 0, 0)', 'blue', 0.5)).toMatchInlineSnapshot(
     `"rgba(0, 0, 255, 0.5)"`
   );
+});
+
+test('mix blue and green', () => {
+  expect(mix('blue', 'green', 0)).toMatchInlineSnapshot(`"rgba(0, 0, 255, 1)"`);
+  expect(mix('blue', 'green', 0.25)).toMatchInlineSnapshot(
+    `"rgba(0, 32, 191, 1)"`
+  );
+  expect(mix('blue', 'green', 0.5)).toMatchInlineSnapshot(
+    `"rgba(0, 64, 128, 1)"`
+  );
+  expect(mix('blue', 'green', 0.75)).toMatchInlineSnapshot(
+    `"rgba(0, 96, 64, 1)"`
+  );
+  expect(mix('blue', 'green', 1)).toMatchInlineSnapshot(`"rgba(0, 128, 0, 1)"`);
 });
--- packages/color2k/src/mix.ts
+++ packages/color2k/src/mix.ts
@@ -14,17 +14,17 @@ function mix(color1: string, color2: string, weight: number): string {
 
   // The formula is copied from the original Sass implementation:
   // http://sass-lang.com/documentation/Sass/Script/Functions.html#mix-instance_method
-  const alphaDelta = a1 - a2;
+  const alphaDelta = a2 - a1;
   const x = weight * 2 - 1;
   const y = x * alphaDelta === -1 ? x : x + alphaDelta;
   const z = 1 + x * alphaDelta;
-  const weight1 = (y / z + 1) / 2.0;
-  const weight2 = 1 - weight1;
+  const weight2 = (y / z + 1) / 2.0;
+  const weight1 = 1 - weight2;
 
   const r = (r1 * weight1 + r2 * weight2) * 255;
   const g = (g1 * weight1 + g2 * weight2) * 255;
   const b = (b1 * weight1 + b2 * weight2) * 255;
-  const a = a1 * weight + a2 * (1 - weight);
+  const a = a2 * weight + a1 * (1 - weight);
 
   return rgba(r, g, b, a);
 }
