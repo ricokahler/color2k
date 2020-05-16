452f7c5bebe4d4e50e57eae9b54bee9d29fe523e
Rico Kahler <ricokahler@me.com>
2020-05-16 19:37:46 -0400

    add demo link (#30)
    * add demo link
    
    * update demo image
    
    * update coverage ignore
    
    * add codecov
    
    * add playwright action
    
    * ignore parse to rgba
    
    * add badges

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,15 @@ module.exports = {
     '^prettier$': '<rootDir>/node_modules/prettier',
   },
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  setupFiles: [`<rootDir>/loadershim.js`],
+  setupFiles: ['<rootDir>/loadershim.js'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/website/',
+    '<rootDir>/scripts/',
+    '<rootDir>/rollup.config.js',
+    // this file isn't reported correctly because the coverage tool can't see
+    // that playwright runs the code in different browsers
+    '<rootDir>/packages/parse-to-rgba/',
+  ],
 };
