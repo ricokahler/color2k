c7fb78d62a382393f60889f9f3e04902c76bbc8f
Rico Kahler <ricokahler@me.com>
2020-05-24 20:00:49 -0400

    rename

--- packages/color2k/src/darken.ts
+++ packages/color2k/src/darken.ts
@@ -4,12 +4,12 @@ import hsla from './hsla';
 /**
  * Darkens using lightness. This is equivalent to subtracting the lightness
  * from the L in HSL.
- * 
+ *
  * @param amount the amount to darken, given as a decimal between 0 and 1
  */
-function lightnessDarken(color: string, amount: number): string {
+function darken(color: string, amount: number): string {
   const [hue, saturation, lightness, alpha] = parseToHsla(color);
   return hsla(hue, saturation, lightness - amount, alpha);
 }
 
-export default lightnessDarken;
+export default darken;
