262f1cb901b8d199f3d46a2993f7066c63ca52c3
Rico Kahler <ricokahler@me.com>
2020-09-06 11:19:34 -0400

    typo

--- src/toHsla.ts
+++ src/toHsla.ts
@@ -2,7 +2,7 @@ import parseToHsla from './parseToHsla';
 import hsla from './hsla';
 
 /**
- * Takes in any color and returns it as a hsla string.
+ * Takes in any color and returns it as an hsla string.
  */
 function toHsla(color: string): string {
   return hsla(...parseToHsla(color));
