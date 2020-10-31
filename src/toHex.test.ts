9ff98468e1352f8af6989927b9a8601070ca602d
Rico Kahler <ricokahler@me.com>
2020-10-30 20:31:09 -0400

    fix: incorrect toHex value (#236)
    * add failing test
    
    * add other scenario
    
    * fix to hex
    
    * copy readme
    
    * bump versions

--- src/toHex.test.ts
+++ src/toHex.test.ts
@@ -1,4 +1,5 @@
 import toHex from './toHex';
+import darken from './darken';
 
 it("takes in any color and returns it's hex code", () => {
   expect(toHex('palevioletred')).toMatchInlineSnapshot(`"#db7093"`);
@@ -14,4 +15,13 @@ test('rgba no transparency', () => {
 
 test('hsla w/ transparency', () => {
   expect(toHex('rgba(0, 255, 0, 0.5)')).toMatchInlineSnapshot(`"#00ff0080"`);
+});
+
+test('https://github.com/ricokahler/color2k/issues/235', () => {
+  expect(darken('#b00020', 0.2)).toMatchInlineSnapshot(
+    `"hsla(349, 100%, 15%, 1)"`
+  );
+  expect(toHex(darken('#b00020', 0.2))).toMatchInlineSnapshot(`"#4d000e"`);
+
+  expect(toHex('#03dac5')).toMatchInlineSnapshot(`"#03dac5"`);
 });
