bb0fe51abc77ba74a378b64d4d6d207a0c10ca5f
DIonysos Dajka <diondiondion@gmail.com>
2020-11-04 15:48:02 +0100

    docs: Improve documentation

--- src/lighten.ts
+++ src/lighten.ts
@@ -3,7 +3,7 @@ import darken from './darken';
  * Lightens a color by a given amount. This is equivalent to
  * `darken(color, -amount)`
  *
- * @param amount the amount to darken, given as a decimal between 0 and 1
+ * @param amount The amount to darken, given as a decimal between 0 and 1
  */
 function lighten(color: string, amount: number): string {
   return darken(color, -amount);
--- src/saturate.ts
+++ src/saturate.ts
@@ -4,8 +4,8 @@ import desaturate from './desaturate';
  * Saturates a color by converting it to `hsl` and increasing the saturation
  * amount. Equivalent to `desaturate(color, -amount)`
  * 
- * @param color the input color
- * @param amount the amount to darken, given as a decimal between 0 and 1
+ * @param color Input color
+ * @param amount The amount to darken, given as a decimal between 0 and 1
  */
 function saturate(color: string, amount: number): string {
   return desaturate(color, -amount);
